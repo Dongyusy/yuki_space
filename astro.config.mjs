8a854fda46de525a09e9b963506ee1ba82e6a148
Dongyusy <1922602696@qq.com>
2026-07-12 19:28:13 +0800

    Configure site for Vercel domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from "astro/config";
 
-const site = "https://dongyusy.github.io";
-const base = "/yuki_space";
+const site = "https://yukiyuan.cn";
 
 export default defineConfig({
   devToolbar: {
     enabled: false,
   },
   site,
-  base,
-  outDir: "docs",
 });
